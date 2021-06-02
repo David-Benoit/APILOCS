fca996a028aa46ab7381cc461e821f0625138405
Manuel David Sánchez Benoit <65616995+David-Benoit@users.noreply.github.com>
2021-06-02 00:46:42 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect(){
-    await mongoose.connect('mongodb://localhost/Locations',{
+    await mongoose.connect('mongodb+srv://fernando:moonigth2000@proyecto.zei7l.mongodb.net/PruebasBD?retryWrites=true&w=majority',{
         useNewUrlParser: true
     });
     console.log('Database: Connected');
